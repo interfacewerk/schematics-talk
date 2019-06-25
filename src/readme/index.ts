2c25945268042d6fefd1201e9b3d80d552ed1816
Felix Lemke <flemke.dev@gmail.com>
2019-06-25 14:24:45 +0200

    fix readme

--- src/readme/index.ts
+++ src/readme/index.ts
@@ -3,8 +3,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function readme(_options: any): Rule {
+export function readme(options: {title: string}): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.overwrite('README.md', options.title)
     return tree;
   };
 }
